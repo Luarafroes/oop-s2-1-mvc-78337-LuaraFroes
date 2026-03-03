93794ab247f60ed1a9b88cff8d34c2170325a200
Luarafroes <luara0511f@gmail.com>
2026-03-03 10:11:17 +0000

    .

--- Library.Tests/UnitTest1.cs
+++ Library.Tests/UnitTest1.cs
@@ -3,9 +3,12 @@ namespace Library.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ThisTestshouldFail()
         {
-
+            string name = "John";
+            Assert.Equal("Luara", name);
         }
+
+  
     }
 }
